c89e5cf8caee66d6c7cfeab87edd73128c1b56c7
misterboya <mady.mister@gmail.com>
2024-07-16 22:32:48 +0100

    added reconcile code

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import { useContext, useState } from "react"
+import { CountContext } from "./components/context";
+import { RecoilRoot, useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
+import { countAtom, evenSelector } from "./store/atoms/count";
+
+function App() {
+
+  return <div>
+    <RecoilRoot>
+      <Count />
+    </RecoilRoot>
+  </div>
+}
+
+function Count() {
+  return <div>
+    <CountRenderer />
+    <Buttons />
+  </div>
+}
+
+function CountRenderer() {
+  const count = useRecoilValue(countAtom);
+  return <div>
+    <b>
+      {count}
+    </b>
+    <EvenCountRenderer />
+  </div>
+}
+
+function EvenCountRenderer() {
+  const isEven = useRecoilValue(evenSelector);
+  return <div>
+    {isEven ? "It is even" : null}
+  </div>
+}
+
+function Buttons() {
+  const setCount = useSetRecoilState(countAtom);
+  console.log("Button re-render");
+
+  return <div>
+    <button onClick={() => {
+      setCount(count => count + 1);
+    }}>Increase</button>
+
+    <button onClick={() => {
+      setCount(count => count - 1);
+    }}>Decrease</button>
+  </div>
+}
+
+export default App
